9c9bdedb2aad9eefb8baa5ca00a818d8ecc0a021
albertopaez <45391490+albertopaez@users.noreply.github.com>
2021-02-19 12:01:50 +0100

    basic header and routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActorsComponent } from './components/actors/actors.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { DirectorsComponent } from './components/directors/directors.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'actors', component: ActorsComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'directors', component: DirectorsComponent },
+  { path: 'login', component: LoginComponent },
+  {path: '', redirectTo: '/login',pathMatch: 'full'},
+  { path        : '**', pathMatch   : 'full', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { ActorsComponent } from './components/actors/actors.component';
+import { DirectorsComponent } from './components/directors/directors.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CategoriesComponent,
+    ActorsComponent,
+    DirectorsComponent,
+    FavoritesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
